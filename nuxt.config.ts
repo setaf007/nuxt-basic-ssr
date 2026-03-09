1fb607af21213e8271a2c08d80bf772c42b72086
Taskin <setaf007@hotmail.com>
2026-03-09 17:08:29 +0800

    performance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,5 +12,17 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/x-icon', href: '/favicon-2.ico' },
       ]
     }
-  }
-})
+  },
+  nitro: {
+    routeRules: {
+      // SSG: Home (static marketing)
+      '/': { prerender: true },
+      
+      // ISR: Products (60s cache, revalidate)
+      '/products': { isr: 60 },
+      '/products/**': { isr: 60 },
+      
+      // Headers for perf
+      '/**': { headers: { 'cache-control': 'public, s-maxage=60, stale-while-revalidate=300' } }
+  },
+}})
